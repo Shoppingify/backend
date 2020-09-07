3b3dc9c4ce79da57d78a6780bfff61eef4c31efe
ips-coding-challenge <coding_challenge@laposte.net>
2020-09-07 12:26:40 +0200

    FEAT: Add categories endpoint

---
+++ src/controllers/categories.controller.js
@@ -0,0 +1,23 @@
+const knex = require('../db/connection')
+
+exports.index = async (ctx) => {
+  try {
+    const categories = await knex('categories').where(
+      'user_id',
+      ctx.state.user.id
+    )
+
+    ctx.status = 200
+    ctx.body = {
+      status: 'success',
+      data: categories,
+    }
+  } catch (e) {
+    ctx.status = e.status || 500
+    ctx.body = {
+      status: 'error',
+      message: e.message,
+    }
+    console.log(`Error`, e)
+  }
+}
