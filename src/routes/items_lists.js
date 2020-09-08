94e3963cb2b9aedece40f5f21a82a7155210990b
ips-coding-challenge <67691276+ips-coding-challenge@users.noreply.github.com>
2020-09-08 11:49:32 +0200

    FIX: Modified the endpoints to add/update/remove an item to a list (#29)

--- src/routes/items_lists.js
+++ src/routes/items_lists.js
@@ -1,10 +1,12 @@
-const Router = require("@koa/router");
-const itemsListsController = require("../controllers/items_lists.controller");
-const jwt = require("../middlewares/jwt");
+const Router = require('@koa/router')
+const itemsListsController = require('../controllers/items_lists.controller')
+const jwt = require('../middlewares/jwt')
 
-const router = new Router();
+const router = new Router()
 
-router.get("/lists/:listId/items", jwt, itemsListsController.index);
-router.post("/lists/:listId/items", jwt, itemsListsController.create);
+router.get('/lists/:listId/items', jwt, itemsListsController.index)
+router.post('/lists/:listId/items', jwt, itemsListsController.create)
+router.put('/lists/:listId/items', jwt, itemsListsController.update)
+router.delete('/lists/:listId/items', jwt, itemsListsController.delete)
 
-module.exports = router;
+module.exports = router
