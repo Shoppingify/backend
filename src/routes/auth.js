9fd3103b330f188ad653db39bf347e6ac763c9af
ips-coding-challenge <67691276+ips-coding-challenge@users.noreply.github.com>
2020-09-14 18:06:26 +0200

    FEAT: Add Github Authentication (#41)
    * FEAT: Addition of github oauth
    
    * FEAT: Addition of github oauth

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,11 +1,13 @@
-const Router = require("@koa/router");
-const authController = require("../controllers/auth.controller");
-const jwt = require("../middlewares/jwt");
+const Router = require('@koa/router')
+const authController = require('../controllers/auth.controller')
+const jwt = require('../middlewares/jwt')
 
-const router = new Router();
+const router = new Router()
 
-router.post("/register", authController.register);
-router.post("/login", authController.login);
-router.get("/me", jwt, authController.me);
+router.post('/register', authController.register)
+router.post('/login', authController.login)
+router.get('/oauth/github/callback', authController.githubOauth)
 
-module.exports = router;
+router.get('/me', jwt, authController.me)
+
+module.exports = router
