82c864c4f6b6e0d85b7cac707d41028b09eb6da4
ips-coding-challenge <67691276+ips-coding-challenge@users.noreply.github.com>
2020-09-27 18:41:01 +0200

    Cleanup some files (#50)

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,19 @@
 // Update with your config settings.
 const path = require('path')
-const env = process.env.NODE_ENV ? process.env.NODE_ENV.trim() : 'development'
+const env = process.env.NODE_ENV ? process.env.NODE_ENV : 'development'
 require('dotenv').config({
   path: path.join(__dirname, `.env.${env}`),
 })
 
 module.exports = {
   development: {
-    client: 'postgresql',
-    connection: 'postgres://postgres:root@localhost:5432/shoppingify',
+    client: 'pg',
+    connection: {
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST,
+    },
     pool: {
       min: 2,
       max: 10,
@@ -22,8 +27,13 @@ module.exports = {
   },
 
   test: {
-    client: 'postgresql',
-    connection: process.env.DATABASE_URL,
+    client: 'pg',
+    connection: {
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST,
+    },
     pool: {
       min: 2,
       max: 10,
@@ -33,22 +43,6 @@ module.exports = {
     },
     seeds: {
       directory: path.join(__dirname, 'src', 'db', 'seeds'),
-    },
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: 'knex_migrations',
     },
   },
 
